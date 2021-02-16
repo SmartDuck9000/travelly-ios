f3b90d4ac726a8ecb66b7006100a58acee0d61f0
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-02-17 00:06:50 +0300

    #1 add file with colors for ui elements

---
+++ Travelly/Services/Theme/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  Travelly
+//
+//  Created by Георгий Куликов on 16.02.2021.
+//
+
+import UIKit
+
+struct MainButtonAppearance {
+    static let backgroundColor = UIColor.systemOrange
+    static let textColor = UIColor.white
+}
+
+struct SpecialButtonAppearance {
+    static let backgroundColor = UIColor.systemBlue
+    static let textColor = UIColor.white
+}
