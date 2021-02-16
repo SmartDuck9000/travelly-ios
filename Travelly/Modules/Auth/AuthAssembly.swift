dcf9e635708f045e046b24ea6469fffde3aa6c73
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-02-16 23:34:36 +0300

    #1 replace register folder to up level and create viper files for auth screen

---
+++ Travelly/Modules/Auth/AuthAssembly.swift
@@ -0,0 +1,8 @@
+//
+//  AuthAssembly.swift
+//  Travelly
+//
+//  Created by Георгий Куликов on 16.02.2021.
+//
+
+import Foundation
---
+++ Travelly/Modules/Auth/AuthRouter.swift
@@ -0,0 +1,8 @@
+//
+//  AuthRouter.swift
+//  Travelly
+//
+//  Created by Георгий Куликов on 16.02.2021.
+//
+
+import Foundation
