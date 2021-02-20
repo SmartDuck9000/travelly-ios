1a4fb98be03f2c9e386904d05dccbcf31fff33fc
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-02-21 02:16:34 +0300

    #1 rename theme directory to style, add file for text field extensions, add text field style settings in app and add some layout for register view

--- Travelly/Modules/Register/RegisterViewController.swift
+++ Travelly/Modules/Register/RegisterViewController.swift
@@ -12,6 +12,7 @@ class RegisterViewController: UIViewController {
     
     private let goBackButton = UIButton()
     
+    private let stackView = UIStackView()
     private let emailField = UITextField()
     private let firstNameField = UITextField()
     private let lastNameField = UITextField()
@@ -31,6 +32,7 @@ class RegisterViewController: UIViewController {
         view.backgroundColor = AppColorAppearance.appBackgroundColor
         
         setupGoBackButton()
+        setupStackView()
         setupEmailField()
         setupFirstNameField()
         setupLastNameField()
@@ -48,23 +50,60 @@ class RegisterViewController: UIViewController {
     }
     
     private func setupGoBackButton() {
+        self.view.addSubview(goBackButton)
+        goBackButton.addTarget(self, action: #selector(goBackButtonPressed), for: .touchUpInside)
         
+        goBackButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setupStackView() {
+        stackView.addArrangedSubview(emailField)
+        stackView.addArrangedSubview(firstNameField)
+        stackView.addArrangedSubview(lastNameField)
+        
+        self.view.addSubview(stackView)
+        
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     private func setupEmailField() {
+        emailField.setLeftPaddingPoints(AuthTextFieldAppearance.leftPadding)
+        emailField.setRightPaddingPoints(AuthTextFieldAppearance.rightPadding)
+        emailField.layer.cornerRadius = view.safeAreaLayoutGuide.layoutFrame.size.width * AuthTextFieldAppearance.cornerRadiusRatio
+        emailField.font = AuthTextFieldAppearance.font
         
+        emailField.translatesAutoresizingMaskIntoConstraints = false
     }
     
     private func setupFirstNameField() {
+        firstNameField.setLeftPaddingPoints(AuthTextFieldAppearance.leftPadding)
+        firstNameField.setRightPaddingPoints(AuthTextFieldAppearance.rightPadding)
+        firstNameField.layer.cornerRadius = view.safeAreaLayoutGuide.layoutFrame.size.width * AuthTextFieldAppearance.cornerRadiusRatio
+        firstNameField.font = AuthTextFieldAppearance.font
         
+        firstNameField.translatesAutoresizingMaskIntoConstraints = false
     }
     
     private func setupLastNameField() {
+        lastNameField.setLeftPaddingPoints(AuthTextFieldAppearance.leftPadding)
+        lastNameField.setRightPaddingPoints(AuthTextFieldAppearance.rightPadding)
+        lastNameField.layer.cornerRadius = view.safeAreaLayoutGuide.layoutFrame.size.width * AuthTextFieldAppearance.cornerRadiusRatio
+        lastNameField.font = AuthTextFieldAppearance.font
         
+        lastNameField.translatesAutoresizingMaskIntoConstraints = false
     }
     
     private func setupRegisterButton() {
+        registerButton.setTitle("Зарегистрироваться", for: .normal)
+        registerButton.setTitleColor(MainButtonAppearance.textColor, for: .normal)
+        registerButton.titleLabel?.font = UIFont(name: MainButtonAppearance.fontName, size: MainButtonAppearance.fontSize)
         
+        registerButton.backgroundColor = MainButtonAppearance.backgroundColor
+        registerButton.layer.cornerRadius = view.safeAreaLayoutGuide.layoutFrame.size.width * MainButtonAppearance.cornerRadiusRatio
+        registerButton.addTarget(self, action: #selector(registerButtonPressed), for: .touchUpInside)
+        
+        registerButton.translatesAutoresizingMaskIntoConstraints = false
     }
 
 }
---
+++ Travelly/Services/Style/TextFields.swift
@@ -0,0 +1,17 @@
+//
+//  TextFields.swift
+//  Travelly
+//
+//  Created by Георгий Куликов on 21.02.2021.
+//
+
+import UIKit
+
+
+struct AuthTextFieldAppearance {
+    static let leftPadding: CGFloat = 15
+    static let rightPadding: CGFloat = 15
+    static let cornerRadiusRatio: CGFloat = 0.02
+    static let font = UIFont.systemFont(ofSize: UIFont.systemFontSize)
+}
+
