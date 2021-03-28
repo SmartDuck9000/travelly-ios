9d1fbaf7f377330a0d6900815cadfc8bfdf98833
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-03-28 17:41:33 +0300

    #3 add to DI service method for 4 arguments

--- Travelly/Services/DependencyService/DependencyContainerProtocol.swift
+++ Travelly/Services/DependencyService/DependencyContainerProtocol.swift
@@ -12,9 +12,11 @@ protocol DependencyContainerProtocol: AnyObject {
     func register<Service, Arg>(service: Service.Type, name: String, factory: @escaping (Arg) -> Service)
     func register<Service, Arg1, Arg2>(service: Service.Type, name: String, factory: @escaping (Arg1, Arg2) -> Service)
     func register<Service, Arg1, Arg2, Arg3>(service: Service.Type, name: String, factory: @escaping (Arg1, Arg2, Arg3) -> Service)
+    func register<Service, Arg1, Arg2, Arg3, Arg4>(service: Service.Type, name: String, factory: @escaping (Arg1, Arg2, Arg3, Arg4) -> Service)
     
     func resolve<Service>(service: Service.Type, name: String) -> Service?
     func resolve<Service, Arg>(service: Service.Type, name: String, argument arg: Arg) -> Service?
     func resolve<Service, Arg1, Arg2>(service: Service.Type, name: String, arguments arg1: Arg1, _ arg2: Arg2) -> Service?
     func resolve<Service, Arg1, Arg2, Arg3>(service: Service.Type, name: String, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3) -> Service?
+    func resolve<Service, Arg1, Arg2, Arg3, Arg4>(service: Service.Type, name: String, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4) -> Service?
 }
--- Travelly/Services/DependencyService/SwinjectContainer.swift
+++ Travelly/Services/DependencyService/SwinjectContainer.swift
@@ -35,6 +35,12 @@ class SwinjectContainer: DependencyContainerProtocol {
         }
     }
     
+    public func register<Service, Arg1, Arg2, Arg3, Arg4>(service: Service.Type, name: String, factory: @escaping (Arg1, Arg2, Arg3, Arg4) -> Service) {
+        container.register(service, name: name) { (_, arg1, arg2, arg3, arg4) -> Service in
+            factory(arg1, arg2, arg3, arg4)
+        }
+    }
+    
     public func resolve<Service>(service: Service.Type, name: String) -> Service? {
         return container.resolve(service, name: name)
     }
@@ -49,5 +55,9 @@ class SwinjectContainer: DependencyContainerProtocol {
     
     public func resolve<Service, Arg1, Arg2, Arg3>(service: Service.Type, name: String, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3) -> Service? {
         return container.resolve(service, name: name, arguments: arg1, arg2, arg3)
+    }
+    
+    public func resolve<Service, Arg1, Arg2, Arg3, Arg4>(service: Service.Type, name: String, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4) -> Service? {
+        return container.resolve(service, name: name, arguments: arg1, arg2, arg3, arg4)
     }
 }
