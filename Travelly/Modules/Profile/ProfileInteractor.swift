5483ab5e6d0fc9d992de31b20a37d42c158ae9be
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-03-28 13:28:33 +0300

    #3 add logic for refreshing token and getting profile data

--- Travelly/Modules/Profile/ProfileInteractor.swift
+++ Travelly/Modules/Profile/ProfileInteractor.swift
@@ -22,7 +22,40 @@ class ProfileInteractor: ProfileInteractorProtocol {
         self.tokens = dataStorageService.getFirst(type: SecurityTokens.self)
     }
     
-    func loadProfile(complition: @escaping (ProfileData?, Error?) -> Void) {
-        
+    func loadProfile(complition: @escaping (ProfileData?, Error?, Bool) -> Void) {
+        networkService.get(query: "/api/users", tokens: tokens, parameters: UserIdData(userId: userId), type: .http) { (data, error, statusCode) in
+            
+            if statusCode == 401 {
+                if let tokens = self.tokens {
+                    self.networkService.refreshToken(query: "api/auth", tokens: tokens, type: .http) { (data, error, statusCode) in
+                        guard let data = data, let authData = try? JSONDecoder().decode(AuthData.self, from: data) else {
+                            self.dataStorageService.deleteFirst(type: SecurityTokens.self)
+                            complition(nil, error, true)
+                            return
+                        }
+                        let tokens: SecurityTokens = SecurityTokens(accessToken: authData.accessToken,
+                                                                    refreshToken: authData.refreshToken)
+                        
+                        self.dataStorageService.updateFirst(entity: tokens)
+                        self.loadProfile(complition: complition)
+                    }
+                } else {
+                    self.dataStorageService.deleteFirst(type: SecurityTokens.self)
+                    complition(nil, error, true)
+                }
+            }
+            
+            if error != nil {
+                complition(nil, error, false)
+                return
+            }
+            
+            guard let data = data, let profileData = try? JSONDecoder().decode(ProfileData.self, from: data) else {
+                complition(nil, error, false)
+                return
+            }
+            
+            complition(profileData, nil, false)
+        }
     }
 }
