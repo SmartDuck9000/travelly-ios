504963bb0eaa4cc317d36d6daf92da05b75f01b6
SmartDuck9000 <kulikovgeorgij000@gmail.com>
2021-03-28 17:39:28 +0300

    #3 add style struct for table view

---
+++ Travelly/Services/Style/Views.swift
@@ -0,0 +1,20 @@
+//
+//  Views.swift
+//  Travelly
+//
+//  Created by Георгий Куликов on 28.03.2021.
+//
+
+import UIKit
+
+struct AppColorAppearance {
+    static let appBackgroundColor = UIColor.systemBackground
+    static let appColor = UIColor.systemOrange
+    static let appTextColor = UIColor.white
+}
+
+struct TableViewCellAppearance {
+    static let backgroungColor = UIColor.systemBackground
+    static let textColor = UIColor.label
+    static let font = UIFont.systemFont(ofSize: UIFont.systemFontSize)
+}
